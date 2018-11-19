e7cd08e498cf5b6079d175d4b4b26610af761ae9
Kevin Lie <kevin.lie@mail.utoronto.ca>
2018-11-18 19:23:20 -0500

    Implemented FrameLayout so that I can add button and other stuff on top of the SurfaceView

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/pong/PongMainActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/pong/PongMainActivity.java
@@ -4,6 +4,11 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import fall2018.csc2017.slidingtiles.R;
 
@@ -17,16 +22,43 @@ public class PongMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pong_game_view);
+
         // Get a Display object to access screen details
         Display display = getWindowManager().getDefaultDisplay();
 
         // Load the resolution into a Point object
         Point size = new Point();
         display.getSize(size);
-        pongView = findViewById(R.id.pongSurfaceView);
+
         // Initialize pongView and set it as the view
         pongView = new PongSurfaceView(this, size.x, size.y);
+
+        //find your frame layout
+        FrameLayout frameLayout = new FrameLayout(this);
+
+        //Adding PongView into frameLayout
+        frameLayout.addView(pongView);
+
+        //Creating button
+        Button button1 = new Button(this);
+        button1.setText("Pause");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PongMainActivity.this, "Paused!", Toast.LENGTH_SHORT).show();
+                }
+        });
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 0, 0, 0);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+
+        //Adding button1 to frameLayout
+        frameLayout.addView(button1, params);
+
+        setContentView(frameLayout);
+
 
 
     }
