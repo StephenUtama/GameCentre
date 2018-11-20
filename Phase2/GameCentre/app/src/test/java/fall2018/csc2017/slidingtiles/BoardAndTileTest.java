68073802a1f0842efa9f8fd5f5abfa6f78cb197c
Stephen <stephen.utama@mail.utoronto.ca>
2018-11-20 16:46:03 -0400

    junit tests for Board Class.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/BoardAndTileTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/BoardAndTileTest.java
@@ -32,14 +32,15 @@ public class BoardAndTileTest {
     }
 
     // TODO: gotta fix this
-//    /**
-//     * Make a solved Board.
-//     */
-//    private void setUpCorrect() {
-//        List<Tile> tiles = makeTiles();
-//        Board gameInfo = new SlidingTilesGameInfo(tiles);
-//        slidingTilesManager = new SlidingTilesManager(gameInfo);
-//    }
+    /**
+     * Make a solved Board.
+     */
+    private void setUpCorrect() {
+        List<Tile> tiles = makeTiles();
+        SlidingTilesGameInfo gameInfo = new SlidingTilesGameInfo();
+        slidingTilesManager = new SlidingTilesManager();
+        slidingTilesManager.setInfo(gameInfo);
+    }
 
     /**
      * Shuffle a few tiles.
@@ -49,53 +50,53 @@ public class BoardAndTileTest {
     }
 
     // TODO: gotta fix this too
-//    /**
-//     * Test whether swapping two tiles makes a solved board unsolved.
-//     */
-//    @Test
-//    public void testIsSolved() {
-//        setUpCorrect();
-//        assertEquals(true, slidingTilesManager.isOver());
-//        swapFirstTwoTiles();
-//        assertEquals(false, slidingTilesManager.isOver());
-//    }
+    /**
+     * Test whether swapping two tiles makes a solved board unsolved.
+     */
+    @Test
+    public void testIsSolved() {
+        setUpCorrect();
+        assertEquals(true, slidingTilesManager.isOver());
+        swapFirstTwoTiles();
+        assertEquals(false, slidingTilesManager.isOver());
+    }
 
     //TODO: you'll see the red squigglies when you uncomment the following code
-//    /**
-//     * Test whether swapping the first two tiles works.
-//     */
-//    @Test
-//    public void testSwapFirstTwo() {
-//        setUpCorrect();
-//        assertEquals(1, slidingTilesManager.getBoard().getTile(0, 0).getId());
-//        assertEquals(2, slidingTilesManager.getBoard().getTile(0, 1).getId());
-//        slidingTilesManager.getBoard().swapTiles(0, 0, 0, 1);
-//        assertEquals(2, slidingTilesManager.getBoard().getTile(0, 0).getId());
-//        assertEquals(1, slidingTilesManager.getBoard().getTile(0, 1).getId());
-//    }
+    /**
+     * Test whether swapping the first two tiles works.
+     */
+    @Test
+    public void testSwapFirstTwo() {
+        setUpCorrect();
+        assertEquals(1, slidingTilesManager.getBoard().getTile(0, 0).getId());
+        assertEquals(2, slidingTilesManager.getBoard().getTile(0, 1).getId());
+        slidingTilesManager.getBoard().swapTiles(0, 0, 0, 1);
+        assertEquals(2, slidingTilesManager.getBoard().getTile(0, 0).getId());
+        assertEquals(1, slidingTilesManager.getBoard().getTile(0, 1).getId());
+    }
 
-//    /**
-//     * Test whether swapping the last two tiles works.
-//     */
-//    @Test
-//    public void testSwapLastTwo() {
-//        setUpCorrect();
-//        assertEquals(15, slidingTilesManager.getBoard().getTile(3, 2).getId());
-//        assertEquals(16, slidingTilesManager.getBoard().getTile(3, 3).getId());
-//        slidingTilesManager.getBoard().swapTiles(3, 3, 3, 2);
-//        assertEquals(16, slidingTilesManager.getBoard().getTile(3, 2).getId());
-//        assertEquals(15, slidingTilesManager.getBoard().getTile(3, 3).getId());
-//    }
-//
-//    /**
-//     * Test whether isValidHelp works.
-//     */
-//    @Test
-//    public void testIsValidTap() {
-//        setUpCorrect();
-//        assertEquals(true, slidingTilesManager.isValidMove(11));
-//        assertEquals(true, slidingTilesManager.isValidMove(14));
-//        assertEquals(false, slidingTilesManager.isValidMove(10));
-//    }
+    /**
+     * Test whether swapping the last two tiles works.
+     */
+    @Test
+    public void testSwapLastTwo() {
+        setUpCorrect();
+        assertEquals(15, slidingTilesManager.getBoard().getTile(3, 2).getId());
+        assertEquals(16, slidingTilesManager.getBoard().getTile(3, 3).getId());
+        slidingTilesManager.getBoard().swapTiles(3, 3, 3, 2);
+        assertEquals(16, slidingTilesManager.getBoard().getTile(3, 2).getId());
+        assertEquals(15, slidingTilesManager.getBoard().getTile(3, 3).getId());
+    }
+
+    /**
+     * Test whether isValidHelp works.
+     */
+    @Test
+    public void testIsValidTap() {
+        setUpCorrect();
+        assertEquals(true, slidingTilesManager.isValidMove(11));
+        assertEquals(true, slidingTilesManager.isValidMove(14));
+        assertEquals(false, slidingTilesManager.isValidMove(10));
+    }
 }
 
