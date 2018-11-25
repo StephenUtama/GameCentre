26cf57a2430548a340a75aa0bef9e24961c64ad8
Kevin Lie <kevin.lie@mail.utoronto.ca>
2018-11-25 18:21:43 -0500

    Edited Team.md

--- Phase2/GameCentre/app/src/test/java/pong/BallTest.java
+++ Phase2/GameCentre/app/src/test/java/pong/BallTest.java
@@ -1,5 +1,16 @@
 package pong;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BallTest {
 
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void reset() {
+    }
 }
