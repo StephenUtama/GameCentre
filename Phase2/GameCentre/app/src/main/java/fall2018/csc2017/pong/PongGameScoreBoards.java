560f2b82014985c09e9de0d8af0bb8a61ab8d354
Yuwa Yokohama <yuwa.yokohama0730@gmail.com>
2018-11-24 22:52:29 -0500

    Solved the bug in Scoreboard updating when the game is done

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/pong/PongGameScoreBoards.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/pong/PongGameScoreBoards.java
@@ -1,12 +1,23 @@
 package fall2018.csc2017.pong;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import generalclasses.GameScoreboards;
+import generalclasses.ScoreBoard;
 
 public class PongGameScoreBoards extends GameScoreboards implements Serializable {
+    private HashMap<String, ScoreBoard> scoreboardsForGame = new HashMap<>();
     public String game;
     PongGameScoreBoards() {
         this.game = "Pong";
+    }
+
+    public ScoreBoard getScoreboard(String gameName) {
+        if (!scoreboardsForGame.keySet().contains(gameName)) {
+            scoreboardsForGame.put(gameName, new PongScoreBoard());
+        }
+        return scoreboardsForGame.get(gameName);
     }
 }
