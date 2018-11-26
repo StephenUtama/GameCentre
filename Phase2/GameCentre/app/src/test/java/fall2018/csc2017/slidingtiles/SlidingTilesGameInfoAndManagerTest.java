9341261b40e83a42d00a202570bc60d30931ef94
Stephen <stephen.utama@mail.utoronto.ca>
2018-11-26 19:29:11 -0400

    Added unittest that test for blanktile position and returning previous position(Undo).

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingTilesGameInfoAndManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingTilesGameInfoAndManagerTest.java
@@ -3,9 +3,14 @@ package fall2018.csc2017.slidingtiles;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
-
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
 
 public class SlidingTilesGameInfoAndManagerTest {
@@ -35,7 +40,8 @@ public class SlidingTilesGameInfoAndManagerTest {
     /**
      * Make a solved Board.
      */
-    private void setUpCorrect() {
+    @Before
+    public void setUpCorrect() {
         List<Tile> tiles = makeTiles();
         SlidingTilesGameInfo gameInfo = new SlidingTilesGameInfo(tiles);
         slidingTilesManager = new SlidingTilesManager();
@@ -44,19 +50,16 @@ public class SlidingTilesGameInfoAndManagerTest {
 
     @Test
     public void testInversionSolvedGame() {
-        setUpCorrect();
         assertEquals(true, slidingTilesManager.getInfo().calculate_inversions(makeTiles(), 15) == 0);
     }
 
     @Test
     public void testGameName() {
-        setUpCorrect();
         assertEquals("Sliding Tiles", slidingTilesManager.getInfo().getGame());
     }
 
     @Test
     public void testCalculateScore() {
-        setUpCorrect();
         slidingTilesManager.makeMove(14);
         assertEquals(1, slidingTilesManager.getInfo().getScore());
         slidingTilesManager.makeMove(15);
@@ -65,7 +68,6 @@ public class SlidingTilesGameInfoAndManagerTest {
 
     @Test
     public void testTouchMove() {
-        setUpCorrect();
         slidingTilesManager.makeMove(14);
         assertEquals(true, slidingTilesManager.getBoard().getTile(3,3).getId() == 15);
         slidingTilesManager.makeMove(12);
@@ -74,7 +76,6 @@ public class SlidingTilesGameInfoAndManagerTest {
 
     @Test
     public void testCoordinatetoIndex() {
-        setUpCorrect();
         int [] ret = new int[2];
         ret[0] = 3;
         ret[1] = 3;
@@ -82,4 +83,21 @@ public class SlidingTilesGameInfoAndManagerTest {
         assertEquals(15, (int) SlidingTilesManager.convertCoordinateToPosition(ret));
     }
 
+    @Test
+    public void testFindBlankTile() {
+        int[] ret = slidingTilesManager.findBlankTile(25);
+        assertEquals(3, ret[0]);
+        assertEquals(3, ret[1]);
+    }
+
+    @Test
+    public void testReturnPreviousMove() throws IndexOutOfBoundsException{
+        SlidingTilesGameInfo gameInfo = mock(SlidingTilesGameInfo.class);
+        slidingTilesManager.setInfo(gameInfo);
+        slidingTilesManager.getInfo().previousMovesList = new ArrayList<>();
+        slidingTilesManager.savePreviousMovePosition(15);
+        int previousMove = slidingTilesManager.returnPreviousMove();
+        assertEquals(15, previousMove);
+        assertTrue(gameInfo.previousMovesList.isEmpty());
+    }
 }
