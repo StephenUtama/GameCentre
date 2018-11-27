2e009fbce9fbc97cf61759fb0db6f776613a8e73
Stephen <stephen.utama@mail.utoronto.ca>
2018-11-27 15:10:50 -0500

    Added unit tests that for Pong game controller that tests different positions of the ball. 91% line coverage for now.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/pong/PongGameControllerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/pong/PongGameControllerTest.java
@@ -38,6 +38,7 @@ public class PongGameControllerTest {
 //        //testInfo.setFps((long)0.25); // So that it will for sure collide with walls
 //        testController = new PongGameController(testInfo);
         testInfo = mock(PongGameInfo.class);
+        testInfo.lives = 2;
         racket = mock(Racket.class);
         ball = mock(Ball.class);
         rectr = mock(RectF.class);
@@ -51,11 +52,12 @@ public class PongGameControllerTest {
         when(ball.getRectF()).thenReturn(rectb);
         doNothing().when(ball).update(isA(Long.class));
         doNothing().when(racket).update(isA(Long.class));
+        when(testInfo.getScreenHeight()).thenReturn(30);
+        when(testInfo.getScreenWidth()).thenReturn(110);
     }
 
     @Test
     public void updateRacketCollision() {
-        testInfo.lives = 2;
         rectr.left = 5;
         rectr.right = 30;
         rectr.bottom = 90;
@@ -69,18 +71,9 @@ public class PongGameControllerTest {
         doNothing().when(ball).clearObstacleY(isA(Float.class));
         doNothing().when(testInfo).updateScore();
         doNothing().when(ball).increaseVelocity();
-        when(testInfo.getScreenHeight()).thenReturn(30);
-        when(testInfo.getScreenWidth()).thenReturn(110);
 
         testController.update();
 
-//        assertTrue(RectF.intersects(testInfo.getRacket().getRectF(), testInfo.getBall().getRectF()));
-//        assertTrue(testInfo.getRacket().getRectF().left < testInfo.getBall().getRectF().right && testInfo.getRacket().getRectF().right > testInfo.getBall().getRectF().left && testInfo.getRacket().getRectF().top < testInfo.getBall().getRectF().bottom && testInfo.getRacket().getRectF().bottom > testInfo.getBall().getRectF().top);
-//
-//        assertTrue(testInfo.getRacket().getRectF().left < testInfo.getBall().getRectF().right);
-//        assertTrue(testInfo.getRacket().getRectF().right > testInfo.getBall().getRectF().left);
-//        assertTrue(testInfo.getRacket().getRectF().top < testInfo.getBall().getRectF().bottom);
-//        assertTrue(testInfo.getRacket().getRectF().bottom > testInfo.getBall().getRectF().top);
         verify(ball).setRandomXVelocity();
         verify(ball).reverseYVelocity();
         verify(ball).clearObstacleY(8);
@@ -96,20 +89,118 @@ public class PongGameControllerTest {
 //        testInfo.getBall().getRectF().top = 30;
 //        testInfo.getBall().getRectF().bottom = 20;
     }
-//    /**
-//     * Returns true iff the two specified rectangles intersect. In no event are
-//     * either of the rectangles modified. To record the intersection,
-//     * use intersect() or setIntersect().
-//     *
-//     * @param a The first rectangle being tested for intersection
-//     * @param b The second rectangle being tested for intersection
-//     * @return true iff the two specified rectangles intersect. In no event are
-//     *              either of the rectangles modified.
-//     */
-//    public static boolean intersects(RectF a, RectF b) {
-//        return a.left < b.right && b.left < a.right
-//                && a.top < b.bottom && b.top < a.bottom;
-//    }
+
+    @Test
+    public void testBottomScreenCollision() {
+        rectr.left = 110;
+        rectr.right = 30;
+        rectr.bottom = 90;
+        rectr.top = 10;
+        rectb.left = 20;
+        rectb.right = 100;
+        rectb.top = 30;
+        rectb.bottom = 40;
+        doNothing().when(ball).reverseYVelocity();
+        doNothing().when(ball).clearObstacleY(isA(Float.class));
+        doNothing().when(testInfo).updateLife();
+
+        testController.update();
+        verify(ball).reverseYVelocity();
+        verify(ball).clearObstacleY(30 - 2);
+        verify(ball, times(0)).reverseXVelocity();
+        verify(ball, times(0)).clearObstacleX(isA(Float.class));
+
+        verify(testInfo).updateLife();
+        verify(testInfo, times(0)).updateScore();
+    }
+
+    @Test
+    public void testTopScreenCollision() {
+        rectr.left = 110;
+        rectr.right = 30;
+        rectr.bottom = 90;
+        rectr.top = 10;
+        rectb.left = 20;
+        rectb.right = 100;
+        rectb.top = -1;
+        rectb.bottom = 20;
+        doNothing().when(ball).reverseYVelocity();
+        doNothing().when(ball).clearObstacleY(isA(Float.class));
+
+        testController.update();
+        verify(ball).reverseYVelocity();
+        verify(ball).clearObstacleY(12);
+        verify(ball, times(0)).reverseXVelocity();
+        verify(ball, times(0)).clearObstacleX(isA(Float.class));
+        assertFalse(testController.isOver());
+    }
+
+    @Test
+    public void testLeftScreenCollision() {
+        rectr.left = 110;
+        rectr.right = 30;
+        rectr.bottom = 90;
+        rectr.top = 10;
+        rectb.left = -1;
+        rectb.right = 100;
+        rectb.top = 15;
+        rectb.bottom = 20;
+        doNothing().when(ball).reverseXVelocity();
+        doNothing().when(ball).clearObstacleX(isA(Float.class));
+
+        testController.update();
+        verify(ball).reverseXVelocity();
+        verify(ball).clearObstacleX(2);
+        verify(ball,times(0)).reverseYVelocity();
+        verify(ball, times(0)).clearObstacleY(isA(Float.class));
+        assertFalse(testController.isOver());
+    }
+
+    @Test
+    public void testRightScreenCollision() {
+        rectr.left = 150;
+        rectr.right = 30;
+        rectr.bottom = 90;
+        rectr.top = 10;
+        rectb.left = 20;
+        rectb.right = 120;
+        rectb.top = 15;
+        rectb.bottom = 20;
+        doNothing().when(ball).reverseXVelocity();
+        doNothing().when(ball).clearObstacleX(isA(Float.class));
+
+        testController.update();
+        verify(ball).reverseXVelocity();
+        verify(ball).clearObstacleX(110-22);
+        verify(ball,times(0)).reverseYVelocity();
+        verify(ball, times(0)).clearObstacleY(isA(Float.class));
+        assertFalse(testController.isOver());
+    }
+
+    @Test
+    public void testSetUpAndRestartAndZeroLives () {
+        doNothing().when(ball).reset(110, 30);
+        testInfo.lives = 0;
+        when(testInfo.getLives()).thenReturn(testInfo.lives);
+        doNothing().when(testInfo).setScore(isA(Integer.class));
+        doNothing().when(testInfo).setLives(isA(Integer.class));
+
+        testController.setupAndRestart();
+
+        verify(testInfo).setScore(0);
+        verify(testInfo).setLives(3);
+    }
+
+    @Test
+    public void testSetUpAndRestartAndLivesRemain () {
+        doNothing().when(ball).reset(110, 30);
+        when(testInfo.getLives()).thenReturn(testInfo.lives);
+
+        testController.setupAndRestart();
+
+        verify(testInfo, times(0)).setScore(0);
+        verify(testInfo, times(0)).setLives(3);
+    }
 
 
 //    @Test
