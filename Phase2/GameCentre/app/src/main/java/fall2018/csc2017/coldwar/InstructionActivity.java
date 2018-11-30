03aeb13242a158c7f9a4b97f8292a8e3c4c002a3
Kailong Huang <kailong.huang@mail.utoronto.ca>
2018-11-30 00:52:19 -0500

    Made instructions scrollable.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/coldwar/InstructionActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/coldwar/InstructionActivity.java
@@ -2,6 +2,8 @@ package fall2018.csc2017.coldwar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import fall2018.csc2017.slidingtiles.R;
 
@@ -11,5 +13,8 @@ public class InstructionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+        TextView instructions = findViewById(R.id.instructions);
+        instructions.setMovementMethod(new ScrollingMovementMethod());
     }
 }
