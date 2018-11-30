3d9d66e0e5b45957224b5537e4d1cd406c4cc06b
Kelvin Fan <kelvinyao.fan@mail.utoronto.ca>
2018-11-30 00:56:02 -0500

    added instructions for cold war

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/coldwar/InstructionActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/coldwar/InstructionActivity.java
@@ -2,6 +2,8 @@ package fall2018.csc2017.coldwar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import fall2018.csc2017.slidingtiles.R;
 
@@ -10,6 +12,9 @@ public class InstructionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_instruction);
+        setContentView(R.layout.activity_coldwar_instructions);
+
+        TextView textView = (TextView) findViewById(R.id.coldwarinstructions);
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
